4d58b192a06b034e65ffd2517c237c69fd7ead67
ROGERS-4 <rogersonyango87@gmail.com>
2026-03-06 18:15:45 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+module.exports = {
+  botName: "QUEEN BELLA",
+  prefix: ".",              // Command prefix
+  apikey: "gifted",         // Your giftedtech API key
+  owner: "+254755660053",  // Example: "254712345678"
+  sessionName: "queen-bella-md", // Session name for WhatsApp connection
+};
